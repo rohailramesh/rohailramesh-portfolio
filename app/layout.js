e88113cb7a2e026558c2f02f6b8741489b2cd694
Rohail Ramesh <rohailramesh@hotmail.com>
2025-04-08 13:35:26 +0100

    Fix: Hotjar in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,6 +13,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="hotjar"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function (c, s, q, u, a, r, e) {
+              c.hj = c.hj || function () { (c.hj.q = c.hj.q || []).push(arguments) };
+              c._hjSettings = { hjid: a };
+              r = s.getElementsByTagName('head')[0];
+              e = s.createElement('script');
+              e.async = true;
+              e.src = q + c._hjSettings.hjid + u;
+              r.appendChild(e);
+            })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 5364960);`,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
@@ -18,3 +37,23 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import { Analytics } from "@vercel/analytics/react";
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata = {
+//   title: "ROHAIL RAMESH",
+//   description: "ROHAIL RAMESH - Software Developer",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>
+//         {children}
+//         <Analytics />
+//       </body>
+//     </html>
+//   );
+// }
--- app/page.js
+++ app/page.js
@@ -4,27 +4,9 @@ import ExperienceTimeline from "@/components/ExperienceTimeLine";
 import Projects from "@/components/Projects";
 import LifeUpdateAndContact from "@/components/LifeUpdateAndContact";
 import Footer from "@/components/Footer";
-import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <Script
-        id="hotjar"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function (c, s, q, u, a, r, e) {
-            c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
-            c._hjSettings = { hjid: a };
-            r = s.getElementsByTagName('head')[0];
-            e = s.createElement('script');
-            e.async = true;
-            e.src = q + c._hjSettings.hjid + u;
-            r.appendChild(e);
-        })(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 5364960);
-        }}
-        `,
-        }}
-      />
       <HeroAnimation />
       <Myself />
       <ExperienceTimeline />
